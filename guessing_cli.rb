25eab835f10cdc550bbc380bec3b5846cfd06c4b
Justin Hutsell <hutselljustin@gmail.com>
2019-05-31 16:22:52 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -9,10 +9,8 @@ def run_guessing_game
     puts "Goodbye"
   elsif input.to_i == correct_num
     puts "You guessed the correct number!"
-    puts "exit"
   else
     puts "The computer guessed #{correct_num}"
-    puts "exit"
   end
   
   
